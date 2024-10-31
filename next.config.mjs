d11b3ad80ba4dbdae9ae8fded712e710a07a14a4
khanaleema <khanaleema770@gmail.com>
2024-10-31 11:46:44 +0500

    Your commit message here

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js configuration options go here
-  reactStrictMode: true, // example option
-  // add more configuration options as needed
+  reactStrictMode: true,
+  trailingSlash: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md'] 
 };
 
-// Exporting the configuration
 export default nextConfig;
